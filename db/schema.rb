a7df87a4235ed6d6314692ba82ecb6b7c6128ff3
Jeffrey <jrhicks@cteh.com>
2016-12-08 09:24:27 -0600

    Install devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711200623) do
+ActiveRecord::Schema.define(version: 20161208151934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "droids", force: :cascade do |t|
-    t.string   "name"
-    t.text     "appears_in"
-    t.string   "primary_function"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer  "from_character_id"
-    t.string   "from_character_type"
-    t.integer  "to_character_id"
-    t.string   "to_character_type"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-  end
-
-  create_table "humen", force: :cascade do |t|
-    t.string   "name"
-    t.text     "appears_in"
-    t.string   "home_planet"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
