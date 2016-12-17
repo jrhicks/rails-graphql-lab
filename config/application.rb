15722ec68fed2e2b047e8bebe634033e742d7a4f
Jeffrey <jrhicks@cteh.com>
2016-12-16 20:27:13 -0700

    flatten folder structure so graphql rails code is closer to the root of the rails app folder

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module GraphqlRubyDemo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.eager_load_paths << Rails.root.join('lib')
-    config.eager_load_paths << Rails.root.join('app', 'graph', 'mutations')
-    config.eager_load_paths << Rails.root.join('app', 'graph', 'types')
-    config.eager_load_paths << Rails.root.join('app', 'graph', 'fields')
+    config.eager_load_paths << Rails.root.join('app', 'mutations')
+    config.eager_load_paths << Rails.root.join('app', 'schema')
+    config.eager_load_paths << Rails.root.join('app', 'types')
   end
 end
